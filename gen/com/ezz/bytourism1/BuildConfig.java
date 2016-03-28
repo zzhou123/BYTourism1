aecc6cbc101972a6ce1a91af2a0446bd73692e63
zzhou123 <591065592@qq.com>
2016-03-28 17:44:13 +0800

    初稿001

---
+++ gen/com/ezz/bytourism1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ezz.bytourism1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/ezz/bytourism1/MainActivity.java
+++ src/com/ezz/bytourism1/MainActivity.java
@@ -1,5 +1,9 @@
 package com.ezz.bytourism1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.R.anim;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
@@ -9,15 +13,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends Activity {
-
+	private TextView textcity;
+	private Spinner spinner;
+	private List<String>list;
+    private ArrayAdapter<String>adapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.choose1);
-
+			
+//		textcity = (TextView)findViewById(R.id.textcity);
+		spinner=(Spinner)findViewById(R.id.spinner1);
+		
+		list=new ArrayList<String>();
+		list.add("北京");
+		list.add("上海");
+		list.add("厦门");
+		
+		adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		
 		
 	}
 }
