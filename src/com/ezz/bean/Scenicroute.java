d2770d6b45ca2fc5a25e28d7ff8c0deefacf1fcf
sysstc <453100473@qq.com>
2016-04-02 10:29:52 +0800

    ByTourism项目更新

---
+++ src/com/ezz/bean/Scenicroute.java
@@ -0,0 +1,77 @@
+package com.ezz.bean;
+
+
+
+public class Scenicroute {
+	private int id;
+	private int one;
+	private int two;
+	private int three;
+	private int four;
+	private int five;
+	public int getFive() {
+		return five;
+	}
+	public void setFive(int five) {
+		this.five = five;
+	}
+	private int cityid;
+	private String describe;
+	private  int avggrade;
+	private int gradenum;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getOne() {
+		return one;
+	}
+	public void setOne(int one) {
+		this.one = one;
+	}
+	public int getTwo() {
+		return two;
+	}
+	public void setTwo(int two) {
+		this.two = two;
+	}
+	public int getThree() {
+		return three;
+	}
+	public void setThree(int three) {
+		this.three = three;
+	}
+	public int getFour() {
+		return four;
+	}
+	public void setFour(int four) {
+		this.four = four;
+	}
+	public int getCityid() {
+		return cityid;
+	}
+	public void setCityid(int cityid) {
+		this.cityid = cityid;
+	}
+	public String getDescribe() {
+		return describe;
+	}
+	public void setDescribe(String describe) {
+		this.describe = describe;
+	}
+	public int getAvggrade() {
+		return avggrade;
+	}
+	public void setAvggrade(int avggrade) {
+		this.avggrade = avggrade;
+	}
+	public int getGradenum() {
+		return gradenum;
+	}
+	public void setGradenum(int gradenum) {
+		this.gradenum = gradenum;
+	}
+	
+}
---
+++ src/com/ezz/bytourism1/SightDetail.java
@@ -0,0 +1,13 @@
+package com.ezz.bytourism1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SightDetail extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sight_detail);
+	}
+}
